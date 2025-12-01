fce4a4f73d7dd9919b0a25f352cb0e9b64c6ada0
griffin <gjstrier@gmail.com>
2025-11-30 21:53:55 -0800

    release: v2.0.3

--- distribution/homebrew/main/bldr.rb
+++ distribution/homebrew/main/bldr.rb
@@ -1,8 +1,8 @@
 class Bldr < Formula
   desc "High-performance build system for mixed-language monorepos"
   homepage "https://github.com/GriffinCanCode/bldr"
-  url "https://github.com/GriffinCanCode/bldr/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "475a999e8645676831a536530e5d2ec60a514db2f18f84dd8b8209e581e92f9e"
+  url "https://github.com/GriffinCanCode/bldr/archive/refs/tags/v2.0.3.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
   license "MIT"
   head "https://github.com/GriffinCanCode/bldr.git", branch: "master"
 
@@ -22,7 +22,7 @@ class Bldr < Formula
 
   test do
     # Test that the binary runs and shows correct version
-    assert_match "bldr version 2.0.0", shell_output("#{bin}/bldr --version")
+    assert_match "bldr version 2.0.3", shell_output("#{bin}/bldr --version")
     
     # Test help command
     system "#{bin}/bldr", "--help"
